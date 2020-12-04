17bc21b0d99dfcc63f1f5b28ab2a3560896807f4
Alex <alexjcosta01@gmail.com>
2020-12-04 14:06:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ db.sequelize
 						
 		// Configuring Express
 		app.use(express.json());						
-		//app.use(cors());
+		app.use(cors());
 		app.options('*', cors())
 
         // Importing routes
