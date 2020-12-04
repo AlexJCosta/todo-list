9c8a5b81c7c0d7b5e566a3154ee43868f68b9fdb
Alex <alexjcosta01@gmail.com>
2020-12-04 09:14:10 -0300

    Update config.js

--- core/config.js
+++ core/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     api_url: process.env.API_URL || 'http://localhost:3000/api',
     port: process.env.DB_PORT || 5432,
-    database: process.env.DB_NAME || 'd33m63q7jpj2uf',
-    username: process.env.DB_USERNAME || 'xfycpuoessqtxb',
-    password: process.env.DB_PASSWORD || '00dc61041b53c921ae8279a711f56b7c308820f542bd3bcfa95259f1d686c606',
-    host: process.env.DB_HOST || 'ec2-34-237-247-76.compute-1.amazonaws.com',
+    database: process.env.DB_NAME || 'dc2lnnrb8c2p4n',
+    username: process.env.DB_USERNAME || 'mecbbxbkwklxtz',
+    password: process.env.DB_PASSWORD || 'e73c7381397a6ec5813eedaaffce9165c0027e37511fd5b31b9ccf9601a5de13',
+    host: process.env.DB_HOST || 'ec2-3-218-123-191.compute-1.amazonaws.com',
     dialect: process.env.DB_DIALECT || 'postgres',
-    connectionString: process.env.DATABASE_URL || 'postgres://xfycpuoessqtxb:00dc61041b53c921ae8279a711f56b7c308820f542bd3bcfa95259f1d686c606@ec2-34-237-247-76.compute-1.amazonaws.com:5432/d33m63q7jpj2uf'
+    connectionString: process.env.DATABASE_URL || 'postgres://mecbbxbkwklxtz:e73c7381397a6ec5813eedaaffce9165c0027e37511fd5b31b9ccf9601a5de13@ec2-3-218-123-191.compute-1.amazonaws.com:5432/dc2lnnrb8c2p4n'
 }
