23dd94dba59dcdff43094249354c1f7c606e62a6
Alex <alexjcosta01@gmail.com>
2020-12-06 19:52:11 -0300

    Adding validator to ToDoItem

--- resources/toDoItem/toDoItem.validator.js
+++ resources/toDoItem/toDoItem.validator.js
@@ -1,13 +1,22 @@
 const validator = require('validator');
-const { update } = require('./toDoItem.controller');
 
 
-const invalidFieldMessages = {}
+const invalidFieldMessages = {
+    nameLength: ["Enter a name with a minimum length of 2 and a maximum of 100 characters."],
+    nameNull: ["Name is required."]
+}
 
 module.exports = {
 
     create(data) {
         let model = {};
+
+        if (data.name != null) {
+            data.name = data.name.replace(/(\s)+/g,' ').trim();
+            model.nameLength = validator.isLength(data.name, {min: 2, max: 100});
+        } else {
+            model.nameNull = false;
+        }
 
         let isValid = true;
         let invalidFields = [];
@@ -27,6 +36,13 @@ module.exports = {
 
     update(data) {
         let model = {};
+
+        if (data.name != null) {
+            data.name = data.name.replace(/(\s)+/g,' ').trim();
+            model.nameLength = validator.isLength(data.name, {min: 2, max: 100});
+        } else {
+            model.nameNull = false;
+        }
 
         let isValid = true;
         let invalidFields = [];
